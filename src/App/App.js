6713ba11e5c52f79dc553bbcbb21aade8e05cf5f
Viktoriya <vfitkovskaya@gmail.com>
2021-04-02 15:04:01 +0300

    Added components Header Main Footer

---
+++ src/App/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import '../common/reset.css'
+import '../common/style.css'
+
+
+
+import Header from './Header/Header'
+import Main from './Main/Main'
+import Footer from './Footer/Footer'
+
+const App=()=>{
+    return(
+        <>
+        <Header/>
+        <Main/>
+        <Footer/>
+     </>
+    )
+}
+
+
+
+export default App
--- src/App/Main/Main.js
+++ src/App/Main/Main.js
@@ -1,82 +1,9 @@
 import React from 'react'
 
-import './reset.css'
-import './style.css'
-import './header.css'
 import './main.css'
 
-const App=()=>{
+const Main = () =>{
     return(
-        <>
-         <header>
-       
-            <nav>
-                <div className="container">
-                    <div className="row top-menu">
-                        <div className="left col-lg-6">
-                            <button className="btn-menu">
-                                <span></span>
-                                <span></span>
-                                <span></span>
-                            </button>
-                            <div className="btn-menu">
-                                Swimming every day
-                            </div>
-                        </div>
-                        <div className="right-end col-lg-6">
-                            <a href="" className="btn btn-link">Home</a>
-
-                            <a href="" className="btn btn-primary btn-rounded btn-login hidden-sm-down"><span>Log in</span>
-                            </a>
-                            <a href="" className="btn btn-secondary btn-rounded hidden-sm-down"><span>Sign up</span>
-                            </a>
-
-                        </div>
-                    </div>
-                </div>
-            </nav>
-        {/* <!-- /Nav --> */}
-
-        {/* <!--Menu--> */}
-        <div className="menu">
-            <ul>
-                <li className="separator"></li>
-                <li>
-                    <a href="">
-                        <span>A Swimmer's life</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="">
-                        <span>Fitness & Wellness</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="">
-                        <span>Insider Advice</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="">
-                        <span>Just for Women</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="">
-                        <span>Swim Tech</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="">
-                        <span>Training & Technique</span>
-                    </a>
-                </li>
-                <li className="separator"></li>
-            </ul>
-        </div>
-        {/* <!--/Menu--> */}
-
-    </header>
     <main>
         <div className="container container-white">
             <div className="container-inner">
@@ -187,11 +114,7 @@ const App=()=>{
             </div>            
         </div>
     </main>
-    <footer >footer</footer>
-     </>
     )
 }
 
-
-
-export default App
+export default Main
