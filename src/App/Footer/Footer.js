8153129c4c41f1bf504fb7da286d5df2f25c4596
Viktoriya <vfitkovskaya@gmail.com>
2021-04-05 21:32:32 +0300

    added footer

--- src/App/Footer/Footer.js
+++ src/App/Footer/Footer.js
@@ -1,9 +1,31 @@
 import React from 'react'
-
+import './footer.css'
+import iconFacebook from './icon_Facebook.png'
+import iconYoutube from './icon_Youtube.png'
+import iconInstagram from './icon_Instagram.png'
+import iconTwitter from './icon_Twitter.png'
 
 const Footer = () =>{
     return(
-    <footer >footer</footer>
+    <footer >
+        <div className="container">
+            <div className="row footer">
+                <div className="logo col-lg-5">
+                    <button>Swimming every day</button>
+                </div>
+                <div className="socialIcons col-lg-7">
+                    <span>Stay In Touch:</span>
+                    <img src={iconFacebook}></img>
+                    <img src={iconYoutube}></img>
+                    <img src={iconInstagram}></img>
+                    <img src={iconTwitter}></img>                
+                </div>
+            </div>
+            <div className="row copyright">
+                 Copyright © 2021
+            </div>
+        </div>
+    </footer>
     )
 }
 
